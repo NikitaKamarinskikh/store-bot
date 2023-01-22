e5481b868a8d9b746c53a0869c660fb627d79b90
nick <n.kamarinskikh@gmail.com>
2023-01-22 19:52:27 +0700

    prosuct models and migrations

--- web/products/admin.py
+++ web/products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Products, Categories, Subcategories
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'subcategory', 'price')
+
+    class Meta:
+        model = Products
+
+
+@admin.register(Categories)
+class CategorieasAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Categories
+
+
+@admin.register(Subcategories)
+class SubcategoriesAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Subcategories
+
+
+
+
--- web/products/models.py
+++ web/products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=60)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Subcategories(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=60)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Подкатегория"
+        verbose_name_plural = "Пдокатегории"
+
+
+class Products(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=255)
+    category = models.ForeignKey(Categories, verbose_name="Категория", on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategories, verbose_name="Подкатегория", on_delete=models.CASCADE)
+    description = models.TextField(verbose_name="Описание")
+    sizes = models.CharField(verbose_name="Размеры", max_length=60)
+    price = models.IntegerField(verbose_name="Цена")
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
