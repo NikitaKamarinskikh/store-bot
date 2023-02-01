68d12aed089ce417bd8de0778e20306b55735be2
nick <n.kamarinskikh@gmail.com>
2023-02-01 11:09:52 +0700

    mailing

--- tasks/check_mailing.py
+++ tasks/check_mailing.py
@@ -1,5 +1,6 @@
+from aiogram import types
 from typing import List
-from web.mailing.models import MailingLists
+from web.mailing.models import MailingLists, MailingListsImages, MailingListsVideos
 from web.clients.models import Clients
 from db_api import clients as clients_model
 from config import ClientsCategories
@@ -10,6 +11,7 @@ async def check_mailing():
     mailings = MailingLists.objects.all()
     for mailing in mailings:
         await _exec_mainling(mailing)
+        mailing.delete()
 
 
 async def _exec_mainling(mailing: MailingLists) -> None:
@@ -39,6 +41,7 @@ async def _exec_mailing_for_clientss_with_no_orders(mailing: MailingLists) -> No
 async def _send_mailing_data(clients: List[Clients], mailing: MailingLists) -> None:
     for client in clients:
         await _send_message(client.telegram_id, mailing.text)
+        await _send_media(client.telegram_id, mailing)
 
 
 async def _send_message(chat_id: int, text: str) -> None:
@@ -49,3 +52,27 @@ async def _send_message(chat_id: int, text: str) -> None:
         )
     except:
         ...
+
+
+async def _send_media(chat_id: int, mailing: MailingLists) -> None:
+    images = MailingListsImages.objects.filter(mailing=mailing)
+    videos = MailingListsVideos.objects.filter(mailing=mailing)
+
+    images_list = [image.telegram_id for image in images]
+    videos_list = [video.telegram_id for video in videos]
+
+    if videos_list or images_list:
+        album = types.MediaGroup()
+        for image in images_list:
+            album.attach_photo(image)
+        for video in videos_list:
+            album.attach_video(video)
+        
+        try:
+            await bot.send_media_group(
+                chat_id=chat_id,
+                media=album
+            )
+        except:
+            ...
+
